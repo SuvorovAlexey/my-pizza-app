a62f9c7508e9c2611bb3fc3c70ed3eedea967ad2
Suvorov Aleksey <suvorov@hostco.ru>
2020-06-08 01:45:24 +0300

    Updates

---
+++ precache-manifest.ded1a5320b5d8ceb6d10f144e7da0670.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06a8a1658c5cdc32ff030959be93e1b",
+    "url": "/my-pizza-app/index.html"
+  },
+  {
+    "revision": "a5f41be79aa3792f3b05",
+    "url": "/my-pizza-app/static/css/main.290e54cd.chunk.css"
+  },
+  {
+    "revision": "b7385d11aed96214c08a",
+    "url": "/my-pizza-app/static/js/2.969762d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-pizza-app/static/js/2.969762d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f41be79aa3792f3b05",
+    "url": "/my-pizza-app/static/js/main.fb1a3a2a.chunk.js"
+  },
+  {
+    "revision": "0fcf51cd78062a0f9aeb",
+    "url": "/my-pizza-app/static/js/runtime-main.ab9ce253.js"
+  },
+  {
+    "revision": "c20a24adc7674282f1213446022b4938",
+    "url": "/my-pizza-app/static/media/1.c20a24ad.jpg"
+  },
+  {
+    "revision": "b8eced656d3cb7097ac92636d3ddb004",
+    "url": "/my-pizza-app/static/media/2.b8eced65.jpg"
+  },
+  {
+    "revision": "9bcbff963a64885394ea37dea8017dfc",
+    "url": "/my-pizza-app/static/media/3.9bcbff96.jpg"
+  },
+  {
+    "revision": "2645e78ed72bf0494bbcc0913eefcbf1",
+    "url": "/my-pizza-app/static/media/4.2645e78e.jpg"
+  },
+  {
+    "revision": "f60eab9444ed7ea2b29765254d51e366",
+    "url": "/my-pizza-app/static/media/5.f60eab94.jpg"
+  },
+  {
+    "revision": "d978f221fe873261e8db42bd810b26d9",
+    "url": "/my-pizza-app/static/media/6.d978f221.jpg"
+  },
+  {
+    "revision": "9c4b0f03185cc0f2baae9b0f04760c7d",
+    "url": "/my-pizza-app/static/media/7.9c4b0f03.jpg"
+  },
+  {
+    "revision": "73a2dad50c27b89b478173fb184fd9f3",
+    "url": "/my-pizza-app/static/media/8.73a2dad5.jpg"
+  },
+  {
+    "revision": "8ecf8c700da391fb0452e4fe30582868",
+    "url": "/my-pizza-app/static/media/9.8ecf8c70.jpg"
+  },
+  {
+    "revision": "9ed9be52253dcd2897d95720fda728ed",
+    "url": "/my-pizza-app/static/media/bg-contacts.9ed9be52.jpg"
+  },
+  {
+    "revision": "78898cf652943d33d9e1efbded4a8cdc",
+    "url": "/my-pizza-app/static/media/bg-menu.78898cf6.png"
+  },
+  {
+    "revision": "7ebdb89d3e3453c19c05a2e860fcea1e",
+    "url": "/my-pizza-app/static/media/fire.7ebdb89d.png"
+  },
+  {
+    "revision": "29fbb4104c5934960dbcb1863e7caeb1",
+    "url": "/my-pizza-app/static/media/pizzamaker.29fbb410.png"
+  }
+]);
